0d26a15ae66078c43055f95f713c2b6094ea97b0
cepthomas <cepthomas@gmail.com>
2022-03-08 19:35:32 -0600

    Update graphics.

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace NebScope
         [STAThread]
         static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new ScopeForm());
